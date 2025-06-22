9782700a96591e1d23b8862841cc196ceefa546e
Nethees <netheeswaran.arunachalam@ucdconnect.ie>
2025-06-22 13:31:03 +0100

    Folder structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Gen AI Medical chatbot",
+    version="0.0.0",
+    author="Netheeswaran A",
+    author_email="netheesarun223@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
